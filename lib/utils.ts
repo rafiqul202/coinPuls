0e0a9c869bf14f29b0ae57df8012abc32322d4d4
Rafiqul Hasan <rafiqulhasan398@gmail.com>
2026-04-23 15:42:51 +0600

    implementation coin detail page section

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,14 +6,27 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 export function formatCurrency(
-  value: number,
-  currency: string = "USD",
-  locale: string = "en-US",
+  value: number | null | undefined,
+  digits?: number,
+  currency?: string,
+  showSymbol?: boolean,
 ) {
-  return new Intl.NumberFormat(locale, {
-    style: "currency",
-    currency: currency,
-  }).format(value);
+  if (value === null || value === undefined || isNaN(value)) {
+    return showSymbol !== false ? "$0.00" : "0.00";
+  }
+
+  if (showSymbol === undefined || showSymbol === true) {
+    return value.toLocaleString(undefined, {
+      style: "currency",
+      currency: currency?.toUpperCase() || "USD",
+      minimumFractionDigits: digits ?? 2,
+      maximumFractionDigits: digits ?? 2,
+    });
+  }
+  return value.toLocaleString(undefined, {
+    minimumFractionDigits: digits ?? 2,
+    maximumFractionDigits: digits ?? 2,
+  });
 }
 
 export function trendingClasses(value: number) {
